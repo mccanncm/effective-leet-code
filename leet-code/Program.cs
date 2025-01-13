9d9d9340edf7a9d2ca944d50cb8574da4bff8912
Caleb McCann <mccanncm66@gmail.com>
2025-01-12 16:37:59 -0800

    two sum - add two sum question and test

---
+++ leet-code/Program.cs
@@ -0,0 +1,10 @@
+﻿using LeetCode.Classes;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var nums = new int[] {2,7,11,15};
+        var foo = TwoSumQuestion.TwoSum(nums, 9);
+    }
+}
